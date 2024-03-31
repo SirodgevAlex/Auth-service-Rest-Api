957c3dba8913a751ee2c2ec8e68ee430e59f8cae
aleksandrsirodzhev <sirodgevolimpwinner@yandex.ru>
2024-03-31 22:04:40 +0300

    work in progress

--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	email string
-	password string
+	Id       int
+	Email    string
+	Password string
 }
 
 var db *sql.DB
+
+var jwtKey = []byte("secret_key")
 
 func main() {
 	var err error
@@ -34,16 +46,41 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/register", register).Methods("POST")
+	router.HandleFunc("/authorize", authorize).Methods("POST")
+	router.HandleFunc("/feed", access_token).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func isEmailValid(email string) {
-
+func isEmailValid(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(email)
 }
 
-func isPasswordSafe(password string) {
-	
+func isPasswordSafe(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+
+	hasUpper := false
+	hasLower := false
+	hasDigit := false
+	hasSpecial := false
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsDigit(char):
+			hasDigit = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	return hasUpper && hasLower && hasDigit && hasSpecial
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
@@ -54,5 +91,118 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE email = $1", user.Email).Scan(&count)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if count > 0 {
+		http.Error(w, "Email уже занят", http.StatusConflict)
+		return
+	}
 
+	if isEmailValid(user.Email) && isPasswordSafe(user.Password) {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		query := "INSERT INTO users(email, password) VALUES($1, $2) RETURNING id"
+		err = db.QueryRow(query, user.Email, hashedPassword).Scan(&user.Id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+type Claims struct {
+	Email string
+	jwt.StandardClaims
+}
+
+func authorize(w http.ResponseWriter, r *http.Request) {
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var hashedPassword string
+	err := db.QueryRow("SELECT password FROM users WHERE email = $1", user.Email).Scan(&hashedPassword)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password)); err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	var user_id int
+	err = db.QueryRow("SELECT Id FROM users WHERE email = $1", user.Email).Scan(&user_id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			Subject: strconv.Itoa(user_id),
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, "Ошибка генерации токена", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+}
+
+func extractUserIdFromToken(r *http.Request) (int, error) {
+	tokenString := r.Header.Get("Authorization")
+	tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return 0, fmt.Errorf("ошибка при парсинге токена: %v", err)
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		user_id, err := strconv.Atoi(claims.Subject)
+		if err != nil {
+			return 0, fmt.Errorf("ошибка при извлечении идентификатора пользователя из токена: %w", err)
+		}
+		return user_id, nil
+	}
+
+	return 0, errors.New("невозможно извлечь идентификатор пользователя из токена")
+}
+
+func access_token(w http.ResponseWriter, r *http.Request) {
+	user_id, err := extractUserIdFromToken(r)
+	if err != nil {
+		http.Error(w, "Пользователь не авторизован", http.StatusUnauthorized)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user_id)
 }
